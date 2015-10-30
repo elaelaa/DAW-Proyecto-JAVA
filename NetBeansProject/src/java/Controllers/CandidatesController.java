9ac7d05ce4a6436ab8f256eda44b646e32e250af
Luis-Lamat <beto_alaska@hotmail.com>
2015-10-30 12:39:36 -0600

    Fixes REGEX that erased the phone param in CandidatesController

--- NetBeansProject/src/java/Controllers/CandidatesController.java
+++ NetBeansProject/src/java/Controllers/CandidatesController.java
@@ -175,7 +175,7 @@ public class CandidatesController extends HttpServlet {
             
         }
         phone = phone.replaceAll(" ", "");
-        phone = phone.replaceAll(".", "");
+        phone = phone.replaceAll("\\.", "");
         phone = phone.replaceAll("-", ""); 
         
         
